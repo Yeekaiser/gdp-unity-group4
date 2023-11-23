cf08264d7e451b1481ab3b357111d9243cea42ec
glitchtrap <140989660+astarrisk@users.noreply.github.com>
2023-11-23 14:38:34 +0800

    Add files via upload
    Sound Meter GUI

---
+++ Assets/SoundMeter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SoundMeter : MonoBehaviour
+{
+    private Image rectangle;
+    private const float MAX_SOUND = 100f;
+    public float sound = MAX_SOUND;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectangle = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectangle.fillAmount = sound / MAX_SOUND;
+    }
+}
