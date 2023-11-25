4caa5290a9f13a81c8a2e3cfede7b002030da89f
Yeekaiser <asura200576@gmail.com>
2023-11-26 01:31:52 +0800

    Adjusted Colliders for Furniture/Player

--- Assets/Scripts/FurnitureDragSound.cs
+++ Assets/Scripts/FurnitureDragSound.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class FurnitureDragSound : MonoBehaviour
 {
+    SoundMeter soundMeter;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //isGrounded = Physics.BoxCast(Vector3.zero, );
     }
 
     // Update is called once per frame
     void Update()
+    {
+        float maxDistance = 10f;
+        RaycastHit hit;
+
+        isGrounded = Physics.BoxCast(transform.position, transform.lossyScale/2, transform.forward, out hit, transform.rotation, maxDistance, 3);
+        if (isGrounded)
+            Debug.Log("On the Ground");
+    }
+
+    private void OnDrawGizmos()
     {
         
     }
