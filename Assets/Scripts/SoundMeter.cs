7de6b2407c5653860437486d6bcd7ae64d408d87
astarrisk <hakerslooshie@gmail.com>
2023-11-30 16:52:34 +0800

    Update SoundMeter.cs

--- Assets/Scripts/SoundMeter.cs
+++ Assets/Scripts/SoundMeter.cs
@@ -8,26 +8,24 @@ public class SoundMeter : MonoBehaviour
 {
     public float currentNoise = 0;
     private Image rectangle;
-    private const float MAX_SOUND = 100f;
+    private const float MAX_SOUND = 1   00f;
     public float sound = MAX_SOUND;
     // Start is called before the first frame update
     void Start()
     {
         rectangle = GetComponent<Image>();
-        InvokeRepeating("decreaseSound", 1.0f, 1.0f);
     }
    
     // Update is called once per frame
     void Update()
     {
         rectangle.fillAmount = currentNoise / MAX_SOUND;
-    }
-    void decreaseSound()
-    {
         if (currentNoise > 0)
         {
-            currentNoise -= 0.5f;
+            currentNoise -= 2f;
         }
+        
     }
+
 
 }
